267ddc631d65f2b8e9993f7f1f22d6e09a509fcf
Milad Samani <Eng.Milad.Samani@gmail.com>
2023-12-02 23:46:26 +0330

    first and last commit

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button :class="mode">
+    <!--We use slots in Vue to send content from the parent into the <template> of a child component.-->
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ["mode"],
+};
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  font-family: inherit;
+  background-color: #3a0061;
+  border: 1px solid #3a0061;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #270041;
+  border-color: #270041;
+}
+
+.flat {
+  background-color: transparent;
+  color: #3a0061;
+  border: none;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: #edd2ff;
+}
+</style>
